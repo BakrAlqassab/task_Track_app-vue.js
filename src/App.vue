a8c5829f69325d0ef6e05494f5bdf90edcf43630
BakrAlqassab <33801792+BakrAlqassab@users.noreply.github.com>
2021-03-24 13:16:55 +0200

    planning the project style

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,19 @@
 <template>
-<h1>Hello world</h1>
-
+<div class="container">
+<Header title='Task Tracker'/>
+ 
+</div>
+  
 </template>
 
 <script>
-
-
+import Header from './components/Header'
 export default {
-  name: 'App',
-  components: {
-
-  }
-}
+  name: "App",
+  components: {Header},
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style  src='./css/crash.css'>
+
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button/>
+  </header>
+</template>
+
+<script>
+import Button from './Button';
+export default {
+  name: "Header",
+  props: {
+      title:String,
+    // title: {
+    //   type: String,
+    //   default: "Hello Finland",
+    // },
+  },
+  components:{
+      Button,
+  }
+};
+</script>
+<style scoped src="../css/header.css"></style>
